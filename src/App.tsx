17d95433b9ff385486a731be310018f1bc3e7b25
Dmitriy Korneev <Dm1korneev@gmail.com>
2020-02-04 11:33:04 +0300

    new change

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App: React.FC = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React TEST 3
+          Learn React TEST 4
         </a>
       </header>
     </div>
