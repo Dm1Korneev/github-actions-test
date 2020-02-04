d94b5d9b8e9a478c2a1482396cb319521a773904
Dm1Korneev <Dm1Korneev@users.noreply.github.com>
2020-02-04 10:48:10 +0000

    Automated deployment: Tue Feb  4 10:48:10 UTC 2020 99667e63e4c64ce04fc78e5bf2ee2850d834a56e

---
+++ precache-manifest.5ba0874940608ca33efd375104eac19f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e654fd235e323b189c07bed9396c31fc",
+    "url": "/github-actions-test/index.html"
+  },
+  {
+    "revision": "e1ed41ec7dbf582cdedb",
+    "url": "/github-actions-test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9b19e68221344a412d80",
+    "url": "/github-actions-test/static/js/2.d168db3d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/github-actions-test/static/js/2.d168db3d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e1ed41ec7dbf582cdedb",
+    "url": "/github-actions-test/static/js/main.e0086e82.chunk.js"
+  },
+  {
+    "revision": "be00c3b9bfb8261bedf2",
+    "url": "/github-actions-test/static/js/runtime-main.45f7d986.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-actions-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0039095b80b653ce8b287e987639e3b7.js"
+  "/github-actions-test/precache-manifest.5ba0874940608ca33efd375104eac19f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github-actions-test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
