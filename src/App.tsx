0b16400a0d0cc47c7d771e0ee9115d27407ad4c2
Dmitriy Korneev <dmitriy.korneev@revolut.com>
2020-11-12 17:59:59 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
+  throw new Error('Test')
+
   return (
     <div className="App">
       <header className="App-header">
